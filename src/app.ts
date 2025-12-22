ff3413e9faa26acdd2761a9925de1cc58dee2434
Andres Felipe Estrada <andresfe1396@gmail.com>
2025-12-22 17:12:43 -0500

    add: express server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("hola mundo");
+import express from "express";
+import { envs } from "../config";
+
+(() => {
+  main();
+})();
+
+function main() {
+  const app = express();
+
+  app.listen(envs.PORT, () => {
+    console.log(`App running on port ${envs.PORT}`);
+  });
+}
