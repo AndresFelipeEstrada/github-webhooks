b89f9e255ac9c62427555b240185a5696ba380ee
Andres Felipe Estrada <andresfe1396@gmail.com>
2026-01-10 15:24:45 -0500

    add: issues endpoint

--- src/presentation/github/controller.ts
+++ src/presentation/github/controller.ts
@@ -14,6 +14,10 @@ export class GithubController {
         message = this.githubService.onStar(payload);
         break;
 
+      case "issues":
+        message = this.githubService.onIssue(payload);
+        break;
+
       default:
         ((message = "Unknown event"), githubEven);
         break;
--- src/presentation/services/github.service.ts
+++ src/presentation/services/github.service.ts
@@ -1,4 +1,4 @@
-import { GitHubStarPayload } from "../../interfaces";
+import type { GitHubIssuePayload, GitHubStarPayload } from "../../interfaces";
 
 export class GitHubService {
   onStar = (payload: GitHubStarPayload): string => {
@@ -7,5 +7,24 @@ export class GitHubService {
     const { sender, action, repository } = payload;
 
     return (message = `User ${sender.login} ${action} star on ${repository.full_name}`);
+  };
+
+  onIssue = (payload: GitHubIssuePayload): string => {
+    let message: string;
+    const { action, issue } = payload;
+
+    if (action === "opened") {
+      return (message = `An issue was opened with this title ${issue.title}`);
+    }
+
+    if (action === "closed") {
+      return (message = `An issue was closed by ${issue.user.login}`);
+    }
+
+    if (action === "reopened") {
+      return (message = `An issue was reopened by ${issue.user.login}`);
+    }
+
+    return `Unhandled action for the issue event ${action}`;
   };
 }
